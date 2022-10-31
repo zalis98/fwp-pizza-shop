e8661dee0fc03c57f8e535ed8a80f49fa0137ed4
Zali Spurgeon s3832759 <s3832759@student.rmit.edu.au>
2022-10-31 18:05:54 +1100

    ordered pizzas display

--- pizza-shop/src/components/Order.js
+++ pizza-shop/src/components/Order.js
@@ -1,20 +1,26 @@
 import "./Order.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 
 function Order() {
 
-  const [pizzas, setPizzas] = useState(JSON.parse(localStorage.getItem("pizzas")));
-  console.log(pizzas);
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    function loadPizzas() {
+      const currentPizzas = JSON.parse(localStorage.getItem("pizzas"));
+      setPizzas(currentPizzas);
+    }
+    loadPizzas();
+  }, [pizzas]);
 
   return (
     <div className="order">
       <h4 className="order-heading">Pizzas order</h4>
       <div className="d-grid gap-2">
         {pizzas.map((x) =>
-        <div>{x.size}</div>)
-    }
-        <Button className="ordered-pizza"></Button>
+          <Button className="ordered-pizza"><p className="pizza-type">{x.size} pizza</p><span className="minus">-</span></Button>
+        )}
         <Button className="add-pizza"><span className="plus">+</span></Button>
       </div>
     </div>
