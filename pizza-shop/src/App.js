37c671a64a3e24c2235b5adace475c71a1ba0803
Zali Spurgeon s3832759 <s3832759@student.rmit.edu.au>
2022-10-31 17:45:25 +1100

    initial pizza order display

--- pizza-shop/src/App.js
+++ pizza-shop/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import PizzaForm from "./components/PizzaForm.js";
+import Order from "./components/Order.js";
 
 function App() {
   return (
     <div className="App">
-      <PizzaForm />
+        <h1 className="pizza-heading">Pizza Form</h1>
+      <div className="display">
+        <PizzaForm />
+        <Order />
+      </div>
     </div>
   );
 }
--- pizza-shop/src/components/Order.js
+++ pizza-shop/src/components/Order.js
@@ -1,7 +1,24 @@
 import "./Order.css";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
 
-function Order () {
+function Order() {
 
+  const [pizzas, setPizzas] = useState(JSON.parse(localStorage.getItem("pizzas")));
+  console.log(pizzas);
+
+  return (
+    <div className="order">
+      <h4 className="order-heading">Pizzas order</h4>
+      <div className="d-grid gap-2">
+        {pizzas.map((x) =>
+        <div>{x.size}</div>)
+    }
+        <Button className="ordered-pizza"></Button>
+        <Button className="add-pizza"><span className="plus">+</span></Button>
+      </div>
+    </div>
+  )
 }
 
 export default Order;
